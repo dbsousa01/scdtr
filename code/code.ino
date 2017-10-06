dbcda8f3459b35d770b05806b7634bc72ec64af3
Jorge Gonçalves <jorgeguilherme95@gmail.com>
2017-10-06 15:12:13 +0100

    Luximeter fixed

--- code/code.ino
+++ code/code.ino
@@ -1,18 +1,18 @@
+#include <math.h>
 
 #define LED 13
 #define LDR A0
 
 float b = 1.808;
 float m = -0.692;
-int R1 = 10000;
+int R1 = 10;
 
 
-float luximeter (int read_val){
+double luximeter (double read_val){
 
-  float F = (R1*(5.0-read_val*5.0/1023.0))/(read_val*5.0/1023.0);
-  float lux_lido = pow(10,(log(F)-b)/m); // o erro é nesta linha, a potencia de 10 da muito pequena
-  Serial.println(log(F));
-  
+  double F = (R1*(5.0-read_val*5.0/1023.0))/(read_val*5.0/1023.0);
+  double lux_lido = pow(10,(log10(F)-b)/m); // o erro é nesta linha, a potencia de 10 da muito pequena
+    
   return(lux_lido);
 }
 
@@ -27,19 +27,19 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  int AR = analogRead(LDR);
-  float lux_lido;
+  double AR = analogRead(LDR);
+  double lux_lido;
 
   digitalWrite(LED, HIGH);
-  delay(100);                       // wait for a second
+  delay(1000);                       // wait for a second
   lux_lido= luximeter(AR);
   //Serial.println(lux_lido);
   //digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
   //delay(1);                       // wait for a second
-  //lux_lido= luximeter(AR);
+  
   
   Serial.print(lux_lido);
   Serial.println(" lux");
-  delay(100);        // delay in between reads for stability
+  delay(1000);        // delay in between reads for stability
 }
 
